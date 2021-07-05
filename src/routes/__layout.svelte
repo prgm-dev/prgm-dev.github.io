59da531134792644a4f39d06c9b09120026d62d5
Alexandre Jouandin <alexandre@jouand.in>
2021-07-06 00:30:01 +0200

    Add Plausible analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,6 +7,11 @@
 <svelte:head>
 	<title>{pageTitle}</title>
 	<meta name="title" content={pageTitle} />
+	<!-- Cookie-free, privacy-preserving analytics by https://plausible.io -->
+	<script
+		defer
+		data-domain="prgm.dev"
+		src="https://plausible.io/js/plausible.js"></script>
 </svelte:head>
 
 <slot />
