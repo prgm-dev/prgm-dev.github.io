134499fc7fcfa21a9a2d98e6ef142947545a3f59
Alexandre Jouandin <alexandre@jouand.in>
2021-07-05 17:13:39 +0200

    Add default Title to website

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import '../app.postcss';
+
+	const pageTitle = 'PRGM Dev';
 </script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+	<meta name="title" content={pageTitle} />
+</svelte:head>
 
 <slot />
