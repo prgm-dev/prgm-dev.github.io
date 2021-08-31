6a99a895464d6945f8389f15a02be11f26ead8b8
Tristan Stérin <tristan.sterin@mu.ie>
2021-08-31 13:37:39 +0100

    Go back to first logo implementation but center with flex

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -7,20 +7,52 @@
 	href="/"
 	class="hoverable:hover:opacity-80 hoverable:active:opacity-100 transition-opacity font-rounded"
 >
-	<span class="" role="img" aria-label="prgm.dev monogram logo">
-		<!-- The Monogram logo -->
-		<img src="/img/PRGM DEV BLANC.svg" />
-	</span></a
->
+	<span class="flex items-center">
+		<span
+			class="inline-block h-text"
+			role="img"
+			aria-label="prgm.dev monogram logo"
+		>
+			<!-- The Monogram logo -->
+			<svg
+				height="100%"
+				viewBox="0 0 360 300"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M230.2 179.9H129.7C124.3 179.9 120 175.5 120 170.2V129.7C120 124.3 124.4 120 129.7 120H230.2C235.6 120 239.9 124.4 239.9 129.7V170.2C240 175.6 235.6 179.9 230.2 179.9Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M350.2 59.9H9.70001C4.30001 59.9 0 55.5 0 50.2V9.69998C0 4.29998 4.40001 0 9.70001 0H350.2C355.6 0 359.9 4.39998 359.9 9.69998V50.2C359.9 55.6 355.6 59.9 350.2 59.9Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M350.2 179.9H309.7C304.3 179.9 300 175.5 300 170.2V129.7C300 124.3 304.4 120 309.7 120H350.2C355.6 120 359.9 124.4 359.9 129.7V170.2C359.9 175.6 355.6 179.9 350.2 179.9Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M350.2 299.9H129.7C124.3 299.9 120 295.5 120 290.2V249.7C120 244.3 124.4 240 129.7 240H350.2C355.6 240 359.9 244.4 359.9 249.7V290.2C359.9 295.6 355.6 299.9 350.2 299.9Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M50.3 179.9H9.79999C4.39999 179.9 0.100006 175.5 0.100006 170.2V129.7C0.100006 124.3 4.49999 120 9.79999 120H50.3C55.7 120 60 124.4 60 129.7V170.2C60 175.6 55.6 179.9 50.3 179.9Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M50.3 299.9H9.79999C4.39999 299.9 0.100006 295.5 0.100006 290.2V249.7C0.100006 244.3 4.49999 240 9.79999 240H50.3C55.7 240 60 244.4 60 249.7V290.2C60 295.6 55.6 299.9 50.3 299.9Z"
+					fill="currentColor"
+				/>
+			</svg>
+		</span><span>prgm</span><span class="text-accent">.dev</span>
+	</span>
+</a>
 
 <style lang="postcss">
 	.h-text {
 		@apply translate-y-1;
 		height: 1em;
 		margin-right: 0.4em;
-	}
-
-	img {
-		width: 220px;
 	}
 </style>
