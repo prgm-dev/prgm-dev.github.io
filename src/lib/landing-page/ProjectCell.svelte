917b805392938ba5c45276e1a16b572fc40d8e1d
Alexandre Jouandin <alexandre@jouand.in>
2022-07-09 00:43:22 +0000

    Rearrange projects

--- src/lib/landing-page/ProjectCell.svelte
+++ src/lib/landing-page/ProjectCell.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	export let href: string;
 	/** Set to `true` when the destination link is trusted */
-	export let referrer = true;
+	export let untrusted = false;
 	export let opensInNewTab = true;
 
 	/** The `rel` parameter for the `a` anchor */
 	let rel: string | null = null;
-	$: rel = opensInNewTab ? 'noopener' + (referrer ? ' noreferrer' : '') : null;
+	$: rel = opensInNewTab ? 'noopener' + (untrusted ? ' noreferrer' : '') : null;
 </script>
 
 <a
@@ -16,9 +16,9 @@
 	class="block w-full xs:w-1/2 sm:w-1/3 flex-shrink-0 text-center px-1 py-2"
 >
 	<h3 class="text-lg font-bold">
-		<slot name="title"><span class="opacity-25">Coming soon...</span></slot>
+		<slot name="title"><span class="text-gray-600">Coming soon...</span></slot>
 	</h3>
-	<p class="opacity-60 text-sm">
+	<p class="text-gray-400 text-sm">
 		<slot name="description"><span>More on that later!</span></slot>
 	</p>
 </a>
